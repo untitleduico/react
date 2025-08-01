8a5cb5138809ba1b1e970d8f5753e672f9ed2618
Dilshod Turobov <deebov@yandex.com>
2025-08-01 16:48:15 +0200

    Rename `featured-icons` to `featured-icon`

--- components/foundations/featured-icon/featured-icon.demo.tsx
+++ components/foundations/featured-icon/featured-icon.demo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CheckCircle } from "@untitledui/icons";
-import { FeaturedIcon } from "@/components/foundations/featured-icon/featured-icons";
+import { FeaturedIcon } from "@/components/foundations/featured-icon/featured-icon";
 
 export const DefaultDemo = () => {
     return (
