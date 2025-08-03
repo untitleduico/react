52f6509ebc7db48454a35801fcd97544ef2e31de
Dilshod Turobov <deebov@yandex.com>
2025-08-03 21:26:15 +0200

    Rename `carousel-primitive` to `carousel-base`

--- components/application/carousel/carousel.demo.tsx
+++ components/application/carousel/carousel.demo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChevronLeft, ChevronRight } from "@untitledui/icons";
-import { Carousel, useCarousel } from "@/components/application/carousel/carousel-primitive";
+import { Carousel, useCarousel } from "@/components/application/carousel/carousel-base";
 import { PaginationDot } from "@/components/application/pagination/pagination-dot";
 
 export const CarouselIndicator = ({
