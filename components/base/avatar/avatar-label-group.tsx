5ef2477a7788202d9e8208055d0d0173108da3bf
Dilshod Turobov <deebov@yandex.com>
2026-03-19 22:44:15 +0000

    Update

--- components/base/avatar/avatar-label-group.tsx
+++ components/base/avatar/avatar-label-group.tsx
@@ -15,12 +15,13 @@ interface AvatarLabelGroupProps extends AvatarProps {
     rounded?: boolean;
     title: string | ReactNode;
     subtitle: string | ReactNode;
+    avatarClassName?: string;
 }
 
-export const AvatarLabelGroup = ({ title, subtitle, className, rounded, ...props }: AvatarLabelGroupProps) => {
+export const AvatarLabelGroup = ({ title, subtitle, className, rounded, avatarClassName, ...props }: AvatarLabelGroupProps) => {
     return (
         <figure className={cx("group flex min-w-0 flex-1 items-center gap-2", className)}>
-            <Avatar border rounded={rounded} {...props} />
+            <Avatar border rounded={rounded} className={avatarClassName} {...props} />
             <figcaption className="min-w-0 flex-1">
                 <p className={cx("font-semibold text-primary", styles[props.size].title)}>{title}</p>
                 <p className={cx("truncate text-tertiary", styles[props.size].subtitle)}>{subtitle}</p>
